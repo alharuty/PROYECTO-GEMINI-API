8a3e11b30bdf1119b0de9528dde1844a7f72a7fa
allharut <132786929+alharuty@users.noreply.github.com>
2024-09-30 21:59:51 +0200

    Creo que funciona

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
+from flask_mysqldb import MySQL
 import os
 import stripe
-import psycopg2
 
 app = Flask(__name__)
 
-# Configuramos CORS
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+# configuramos CORS
 CORS(app)
 
-# Configuramos la conexión a la base de datos PostgreSQL
-db_url = os.getenv('DATABASE_URL')  # Obtiene la URL de conexión de Heroku
+stripe.api_key = "sk_test_51Q290kH0Oxn0trnELbauIxm7bQHVujWZTtRA1F5QWcxG0iOnhrkBzd6OZ3CacW7wJ9Cgfz2RJBSRxZTS3EtfThya00K9sMQ5QJ"
 
-# Conectar a la base de datos PostgreSQL
-try:
-    conn = psycopg2.connect(db_url)
-    cursor = conn.cursor()
-except Exception as e:
-    print(f"Error de conexión a la base de datos: {e}")
+
+# configuramos MySQL Workbench
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Trikitiki22.'
+app.config['MYSQL_DB'] = 'gemini-db'
+
+mysql = MySQL(app)
+
 
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -34,75 +41,67 @@ def allowed_file(filename):
 ############## RUTA PARA OBTENER TODOS LOS PRODUCTOS ############################
 @app.route('/api/productos', methods=['GET'])
 def get_productos():
-    try:
-        cursor.execute("SELECT * FROM \"gemini-productos\"")
-        productos = cursor.fetchall()
-        lista_productos = []
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM `gemini-productos`")
+    productos = cur.fetchall()
+    lista_productos = []
 
-        for producto in productos:
-            imagen = producto[3]
-            if isinstance(imagen, bytes):
-                imagen = imagen.decode('utf-8')
-            imagen = imagen.strip()
+    for producto in productos:
+        imagen = producto[3]
+        if isinstance(imagen, bytes):
+            imagen = imagen.decode('utf-8')
+        imagen = imagen.strip()
 
-            lista_productos.append({
-                'id': producto[0],
-                'nombre': producto[1],
-                'precio': producto[2],
-                'imagen': f'https://gemini-db-82cf11702797.herokuapp.com/uploads/{imagen}',
-                'cantidadStock': producto[4],
-                'color': producto[5],
-                'descripcion': producto[6],
-                'material': producto[7],
-                'descuento': producto[8],
-                'porcentajeDescuento': producto[9],
-                'genero': producto[10]
-            })
+        lista_productos.append({
+            'id': producto[0],
+            'nombre': producto[1],
+            'precio': producto[2],
+            'imagen': f'http://127.0.0.1:5000/uploads/{imagen}',
+            'cantidadStock': producto[4],
+            'color': producto[5],
+            'descripcion': producto[6],
+            'material': producto[7],
+            'descuento': producto[8],
+            'porcentajeDescuento': producto[9],
+            'genero': producto[10]
+        })
 
-        return jsonify(lista_productos)
-
-    except Exception as e:
-        print(f"Error al obtener productos: {e}")
-        return jsonify({'mensaje': 'Error al obtener productos'}), 500
-    finally:
-        cursor.close()
+    cur.close()
+    return jsonify(lista_productos)
 ################################################################################
 
 
 ############# RUTA PARA OBTENER UN PRODUCTO POR ID ##############################
 @app.route('/api/productos/<int:id>', methods=['GET'])
 def get_producto(id):
-    try:
-        cursor.execute("SELECT * FROM \"gemini-productos\" WHERE id = %s", (id,))
-        producto = cursor.fetchone()
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM `gemini-productos` WHERE id = %s", (id,))
+    producto = cur.fetchone()
 
-        if producto:
-            imagen = producto[3]
-            if isinstance(imagen, bytes):
-                imagen = imagen.decode('utf-8')
+    if producto:
 
-            producto_obj = {
-                'id': producto[0],
-                'nombre': producto[1],
-                'precio': producto[2],
-                'imagen': f'https://gemini-db-82cf11702797.herokuapp.com/uploads/{imagen}',
-                'cantidadStock': producto[4],
-                'color': producto[5],
-                'descripcion': producto[6],
-                'material': producto[7],
-                'descuento': producto[8],
-                'porcentajeDescuento': producto[9],
-                'genero': producto[10]
-            }
-            return jsonify(producto_obj)
-        else:
-            return jsonify({'mensaje': 'Producto no encontrado'}), 404
-
-    except Exception as e:
-        print(f"Error al obtener el producto: {e}")
-        return jsonify({'mensaje': 'Error al obtener el producto'}), 500
-    finally:
-        cursor.close()
+        imagen = producto[3]
+        if isinstance(imagen, bytes):
+            imagen = imagen.decode('utf-8')
+        
+        producto_obj = {
+            'id': producto[0],
+            'nombre': producto[1],
+            'precio': producto[2],
+            'imagen': f'http://127.0.0.1:5000/uploads/{imagen}',
+            'cantidadStock': producto[4],
+            'color': producto[5],
+            'descripcion': producto[6],
+            'material': producto[7],
+            'descuento': producto[8],
+            'porcentajeDescuento': producto[9],
+            'genero': producto[10]
+        }
+        cur.close()
+        return jsonify(producto_obj)
+    else:
+        cur.close()
+        return jsonify({'mensaje': 'Producto no encontrado'}), 404
 ################################################################################
 
 
@@ -116,10 +115,10 @@ def add_producto():
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         imagen.save(file_path)
 
-        nombre = request.form.get('nombre')  # campo obligatorio
-        precio = request.form.get('precio')  # campo obligatorio
-        cantidadStock = request.form.get('cantidadStock')  # campo obligatorio
-        color = request.form.get('color')  # campo obligatorio
+        nombre = request.form.get('nombre') # campo obligatorio
+        precio = request.form.get('precio') # campo obligatorio
+        cantidadStock = request.form.get('cantidadStock') # campo obligatorio
+        color = request.form.get('color') # campo obligatorio
         descripcion = request.form.get('descripcion')  # campo opcional
         material = request.form.get('material')  # campo obligatorio
         descuento = request.form.get('descuento', 0)  # por defecto 0 (sin descuento)
@@ -135,59 +134,55 @@ def add_producto():
             descuento = int(descuento)
             porcentajeDescuento = int(porcentajeDescuento)
 
-            cursor.execute("""
-                INSERT INTO \"gemini-productos\" 
+            cur = mysql.connection.cursor()
+            cur.execute("""
+                INSERT INTO `gemini-productos` 
                 (nombre, precio, imagen, cantidadStock, color, descripcion, material, descuento, porcentajeDescuento, genero) 
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """, (nombre, precio, filename, cantidadStock, color, descripcion, material, descuento, porcentajeDescuento, genero))
-            conn.commit()
+            mysql.connection.commit()
+            cur.close()
 
             return jsonify({'mensaje': 'Producto añadida con éxito'}), 201
 
         except ValueError:
             return jsonify({'mensaje': 'Error en el formato de los datos numéricos (precio, cantidadStock, descuento, porcentajeDescuento)'}), 400
-        except Exception as e:
-            conn.rollback()
-            return jsonify({'mensaje': 'Error al agregar el producto', 'error': str(e)}), 500
 
     else:
-        return jsonify({'mensaje': 'Hubo algún error...'}), 400
+        return jsonify({'mensaje': 'Hubo algun error...'}), 400
 ################################################################################
 
-
+ 
 ############### RUTA PARA ELIMINAR UN PRODUCTO #################################
 @app.route('/api/productos/<int:id>', methods=['DELETE'])
 def delete_producto(id):
-    try:
-        # verificamos si la joya existe
-        cursor.execute("SELECT * FROM \"gemini-productos\" WHERE id = %s", (id,))
-        producto = cursor.fetchone()
-
-        if producto:
-            cursor.execute("DELETE FROM \"gemini-productos\" WHERE id = %s", (id,))
-            conn.commit()
-            return jsonify({'mensaje': 'Producto eliminado con éxito'}), 200
-        else:
-            return jsonify({'mensaje': 'Producto no encontrado'}), 404
-
-    except Exception as e:
-        conn.rollback()
-        return jsonify({'mensaje': 'Error al eliminar el producto', 'error': str(e)}), 500
-    finally:
-        cursor.close()
+    cur = mysql.connection.cursor()
+    
+    # verificamos si la joya existe
+    cur.execute("SELECT * FROM `gemini-productos` WHERE id = %s", (id,))
+    producto = cur.fetchone()
+    
+    if producto:
+        cur.execute("DELETE FROM `gemini-productos` WHERE id = %s", (id,))
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({'mensaje': 'Producto eliminado con éxito'}), 200
+    else:
+        cur.close()
+        return jsonify({'mensaje': 'Producto no encontrado'}), 404
 ################################################################################
-
+    
 
 ############## Ruta para subir imágenes ########################################
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return jsonify({'mensaje': 'No se eligió ninguna imagen'}), 400
-
+    
     file = request.files['file']
     if file.filename == '':
         return jsonify({'mensaje': 'No se seleccionó ningún archivo'}), 400
-
+    
     if file and allowed_file(file.filename):
         filename = file.filename
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
@@ -204,7 +199,7 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 ################################################################################
 
-
+    
 ############## RUTA PARA COMPRAR UN PRODUCTO ##################################
 @app.route('/checkout', methods=['POST'])
 def checkout():
@@ -230,50 +225,65 @@ def checkout():
 
         session = stripe.checkout.Session.create(
             payment_method_types=['card'],
-            line_items=data['items'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'eur',
+                    'product_data': {'name': item['nombre']},
+                    'unit_amount': int(item['precio'] * 100),
+                },
+                'quantity': item['cantidadStock'],
+            } for item in data['items']],
             mode='payment',
-            success_url='https://gemini-db-82cf11702797.herokuapp.com/success',
-            cancel_url='https://gemini-db-82cf11702797.herokuapp.com/cancel',
-            metadata={
-                'nombre': cliente['nombre'],
-                'apellidos': cliente['apellidos'],
-                'email': cliente['email'],
-                'telefono': cliente['telefono'],
-                'direccion': f"{cliente['tipoVia']} {cliente['nombreCalle']} {cliente['numeroCalle']} {cliente['pisoPuerta']}, {cliente['codigoPostal']} {cliente['ciudad']}, {cliente['provincia']}, {cliente['pais']}"
+            success_url='http://localhost:3000/pago-completado',
+            cancel_url='http://localhost:3000/pago-cancelado',
+            payment_intent_data={
+                'metadata': {
+                    'nombre': cliente['nombre'],
+                    'apellidos': cliente['apellidos'],
+                    'email': cliente['email'],
+                    'telefono': cliente['telefono'],
+                    'direccion': f"{cliente['tipoVia']} {cliente['nombreCalle']} {cliente['numeroCalle']} {cliente['pisoPuerta']}, {cliente['codigoPostal']} {cliente['ciudad']}, {cliente['provincia']}, {cliente['pais']}"
+                }
             }
         )
+        return jsonify({'url': session.url})
 
-        return jsonify({'id': session.id})
-
+    except stripe.error.StripeError as e:
+        return jsonify({'error': f'Error de Stripe: {str(e)}'}), 500
     except Exception as e:
-        print(f"Error en la creación de sesión de pago: {e}")
-        return jsonify({'error': str(e)}), 500
+        print(f"Error en el checkout: {e}")
+        return jsonify({'error': f'Ocurrió un error: {str(e)}'}), 500
 ################################################################################
+
 
 ############### RUTA PARA CREAR UN NUEVO GÉNERO ################################
 @app.route('/api/generos', methods=['POST'])
 def create_genero():
     try:
+        cur = mysql.connection.cursor()
+        
         # Verificar que se ha subido una imagen
         if 'imagen_genero' not in request.files:
             return jsonify({'error': 'No se ha subido ninguna imagen'}), 400
         
         imagen_genero = request.files['imagen_genero']
         
+        id = request.form.get('id')
         nombre_genero = request.form.get('nombre_genero')
 
-        if not all([nombre_genero, imagen_genero]):
+        if not all([id, nombre_genero, imagen_genero]):
             return jsonify({'error': 'Faltan campos requeridos'}), 400
 
         ruta_imagen = os.path.join(app.config['UPLOAD_FOLDER'], imagen_genero.filename)
         imagen_genero.save(ruta_imagen)
 
-        # Insertar en la base de datos
-        cursor.execute(
-            "INSERT INTO generos (nombre_genero, imagen_genero) VALUES (%s, %s)",
-            (nombre_genero, ruta_imagen)
+
+        cur.execute(
+            "INSERT INTO `generos` (id, nombre_genero, imagen_genero) VALUES (%s, %s, %s)",
+            (id, nombre_genero, ruta_imagen)
         )
-        conn.commit()
+        mysql.connection.commit()
+        cur.close()
 
         return jsonify({'message': 'Género creado exitosamente'}), 201
 
@@ -285,24 +295,24 @@ def create_genero():
 ############## RUTA PARA OBTENER TODOS LOS GÉNEROS QUE TENEMOS ####################
 @app.route('/api/generos', methods=['GET'])
 def get_generos():
-    cursor.execute("SELECT * FROM generos")
-    generos = cursor.fetchall()
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM `generos`")
+    generos = cur.fetchall()
     generos_list = []
 
     for genero in generos:
-        imagen = genero[2]  # La ruta de la imagen está almacenada en el campo 2 (imagen_genero)
-        
-        # Crear una URL accesible para la imagen
-        imagen_url = f"https://gemini-db-82cf11702797.herokuapp.com/uploads/{os.path.basename(imagen)}"
-        
+        imagen = genero[2]
+        if isinstance(imagen, bytes):
+            imagen = imagen.decode('utf-8')
+        imagen = imagen.strip()
         generos_list.append({
             'id': genero[0],
             'nombre_genero': genero[1],
-            'imagen_genero': imagen_url,
+            'imagen_genero': f'http://127.0.0.1:5000/{imagen}',
         })
 
+    cur.close()
     return jsonify(generos_list)
-
 ################################################################################
 
 
@@ -310,13 +320,18 @@ def get_generos():
 ################ RUTA PARA EDITAR UN PRODUCTO ###################################
 @app.route('/api/productos/<int:id>/edit', methods=['PUT'])
 def edit_producto(id):
-    cursor.execute("SELECT * FROM gemini-productos WHERE id = %s", (id,))
-    producto = cursor.fetchone()
+    cur = mysql.connection.cursor()
+
+    cur.execute("SELECT * FROM `gemini-productos` WHERE id = %s", (id,))
+    producto = cur.fetchone()
 
     if not producto:
+        cur.close()
         return jsonify({'mensaje': 'Producto no encontrado'}), 404
 
+
     if request.method == 'PUT' and request.content_type.startswith('multipart/form-data'):
+
         nombre = request.form.get('nombre', producto[1])
         precio = float(request.form.get('precio', producto[2]))
         cantidadStock = int(request.form.get('cantidadStock', producto[4]))
@@ -328,27 +343,25 @@ def edit_producto(id):
         genero = request.form.get('genero', producto[10])
 
         try:
-            cursor.execute(""" 
-                UPDATE gemini-productos 
+            cur.execute(""" 
+                UPDATE `gemini-productos` 
                 SET nombre = %s, precio = %s, cantidadStock = %s, color = %s, 
                     descripcion = %s, material = %s, descuento = %s, 
                     porcentajeDescuento = %s, genero = %s 
                 WHERE id = %s
             """, (nombre, precio, cantidadStock, color, descripcion, material, descuento, porcentajeDescuento, genero, id))
 
-            conn.commit()
+            mysql.connection.commit()
+            cur.close()
             return jsonify({'mensaje': 'Producto actualizado con éxito'}), 200
 
         except Exception as e:
-            conn.rollback()
+            mysql.connection.rollback()
+            cur.close()
             return jsonify({'mensaje': 'Error al actualizar el producto', 'error': str(e)}), 500
 
     return jsonify({'mensaje': 'Tipo de contenido no soportado'}), 415
 ################################################################################
 
-
-
-
-# Levantamos el servidor
 if __name__ == '__main__':
     app.run(debug=True)
